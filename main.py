66afbde62f49f48b78c7a44314f243614ccb2d33
AllysonCui <allyson.cui@mail.utoronto.ca>
2024-07-23 06:25:50 -0400

    Moved the result processing functions out from main.py to combine_results.py.

--- main.py
+++ main.py
@@ -98,30 +98,6 @@ def main(github_df, base_dir="client_projects", start=0, end=None):
     return annotated_df, invalid_df
 
 
-def combine_csvs(some_csv):
-    df = pd.DataFrame()
-    for i in range(1, 12):
-        path = some_csv + "_" + str(i) + ".csv"
-        small_df = pd.read_csv(os.path.join("data", path))
-        df = pd.concat([df, small_df], ignore_index=True)
-    df.to_csv(os.path.join("data", some_csv + ".csv"), index=False)
-
-
-def delete_small_csvs(some_csv, count):
-    for i in range(1, count + 1):
-        path = some_csv + "_" + str(i) + ".csv"
-        full_path = os.path.join("data", path)
-        os.remove(full_path)
-        print(f"Deleted {full_path}")
-
-
-def clear_directory(directory):
-    for file_name in os.listdir(directory):
-        file_path = os.path.join(directory, file_name)
-        os.remove(file_path)
-        print(f"Deleted {file_path}")
-
-
 if __name__ == "__main__":
     articles = pd.read_csv(os.path.join("data", "scientific_data_articles.csv"))
 
@@ -141,14 +117,5 @@ if __name__ == "__main__":
         invalid_df.to_csv(os.path.join("data",
                                        f"invalid_scientific_data_articles_{count}.csv"), index=False)
 
-    # Combine the results
-    combine_csvs("annotated_scientific_data_articles")
-    combine_csvs("invalid_scientific_data_articles")
-
-    # Some cloned repos are not deleted from the client_projects due to error
-    clear_directory("client_projects")
-
-    # Remove the small csvs
-    delete_small_csvs("annotated_scientific_data_articles", count)
-    delete_small_csvs("invalid_scientific_data_articles", count)
+    # Go to combine_results.py
 
